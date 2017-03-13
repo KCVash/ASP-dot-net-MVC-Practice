bfe961046e53f6e66580a316d9352d475894718e
KCHsu <kchsu0419@gmail.com>
2017-03-13 21:31:20 +0800

    [TDD][Day 24] BDD, SpecFlow Introduction

---
+++ BDD_SpecFlow_Introduction/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace BDD_SpecFlow_Introduction
+{
+    public class Calculator
+    {
+        public int FirstNumber {private get; set; }
+        public int SecondNumber {private get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }   
+    }
+}
---
+++ BDD_SpecFlow_Introduction/CalculatorSteps.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace BDD_SpecFlow_Introduction
+{
+    [Binding]
+    public sealed class CalculatorSteps
+    {
+        private int result { get; set; }
+        private Calculator calculator = new Calculator();
+
+        // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see http://go.specflow.org/doc-sharingdata 
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            calculator.FirstNumber = number;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see http://go.specflow.org/doc-sharingdata 
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            calculator.SecondNumber = number;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            //TODO: implement act (action) logic
+
+            result = calculator.Add();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            //TODO: implement assert (verification) logic
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
